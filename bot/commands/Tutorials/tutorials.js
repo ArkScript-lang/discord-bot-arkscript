5ea5015f12c7bbc346a8039f6eecd37e25f55540
Kuriioo <61805275+Kuriioo@users.noreply.github.com>
2020-08-21 17:41:44 +0200

    Update tutorial.js
    MessageEmbed héhé

--- bot/commands/Tutorials/tutorials.js
+++ bot/commands/Tutorials/tutorials.js
@@ -1,25 +1,23 @@
 'use strict';
 
 require('dotenv').config();
-
+const Discord = require("discord.js");
 exports.run = (client, msg, args) => {
-    msg.channel.send({
-        embed: {
-            color: 0x6666ff,
-            title: 'Tutorials',
-            description: 'A list of commands to launch to get short tutorials in ArkScript',
-            fields: [
-                {
-                    name: 'tuto0',
-                    value: 'Getting started',
-                },
-                {
-                    name: 'tuto1',
-                    value: 'Program structure, printing and conditions',
-                },
-            ],
-        }
-    });
+    msg.channel.send(new Discord.MessageEmbed({
+        color: 0x6666ff,
+        title: 'Tutorials',
+        description: 'A list of commands to launch to get short tutorials in ArkScript',
+        fields: [
+            {
+                name: 'tuto0',
+                value: 'Getting started',
+            },
+            {
+                name: 'tuto1',
+                value: 'Program structure, printing and conditions',
+            },
+         ],
+    }));
 };
 
 exports.help = {
