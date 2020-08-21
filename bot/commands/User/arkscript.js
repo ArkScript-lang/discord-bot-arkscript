535d9229ddd18f3e7bb058eeab1fca716b1ac12d
Kuriioo <61805275+Kuriioo@users.noreply.github.com>
2020-08-21 17:29:34 +0200

    Update arkscript.js
    Plus simple comme ça.

--- bot/commands/User/arkscript.js
+++ bot/commands/User/arkscript.js
@@ -1,26 +1,25 @@
 'use strict';
+const Discord = require("discord.js");
 
 exports.run = (client, msg, args) => {
-    msg.channel.send({
-        embed: {
-            title: 'ArkScript',
-            description: 'A small, fast, functional and scripting language for video games',
-            color: 0x6666ff,
-            fields: [
-                {
-                    name: 'GitHub',
-                    value: 'https://github.com/ArkScript-lang',
-                },
-                {
-                    name: 'Website',
-                    value: 'https://arkscript-lang.github.io',
-                },
-            ],
-            footer: {
-                text: 'Made by SuperFola#5854',
-            }
-        }
-    });
+    msg.channel.send(new Discord.MessageEmbed({
+        title: 'ArkScript',
+        description: 'A small, fast, functional and scripting language for video games',
+        color: 0x6666ff,
+        fields: [
+            {
+                name: 'GitHub', 
+                value: 'https://github.com/ArkScript-lang',
+            },
+            {
+                name: 'Website',
+                value: 'https://arkscript-lang.github.io',
+            },
+         ],
+         footer: {
+            text: 'Made by SuperFola#5854',
+         }
+    }));
 };
 
 exports.help = {
