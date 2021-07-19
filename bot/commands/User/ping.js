68bdb0c7349e68d8aa3523ff28ab2b773866c26d
Alexandre Plateau <lexplt.dev@gmail.com>
2021-07-19 19:08:17 +0200

    enhancing ping command

--- bot/commands/User/ping.js
+++ bot/commands/User/ping.js
@@ -3,22 +3,26 @@
 require('dotenv').config();
 
 exports.run = async (client, msg, args) => {
-    let msg_ping = await msg.channel.send('...');
+    let embed = (ping, api_ping) => {
+        return {
+            embed: {
+                title: 'Ping',
+                description: `${Math.round(ping)}ms`,
+                color: 0x11dd11,
+                fields: [
+                    {
+                        name: 'API',
+                        value: `${api_ping}ms`,
+                    },
+                ],
+            },
+        };
+    };
+
+    let msg_ping = await msg.channel.send(embed(NaN, NaN));
     let ping = msg_ping.createdTimestamp - msg.createdTimestamp;
 
-    await msg_ping.edit({
-        embed: {
-            title: 'Ping',
-            description: `${Math.round(ping)}ms`,
-            color: 0x11dd11,
-            fields: [
-                {
-                    name: 'API',
-                    value: `${client.ws.ping}ms`,
-                },
-            ],
-        },
-    });
+    await msg_ping.edit(embed(ping, client.ws.ping));
 };
 
 exports.help = {
